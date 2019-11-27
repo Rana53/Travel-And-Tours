cc4ad04440dc12b33511c2871dd41b53ccc9c8cd
Rana53 <ranajahid.cseru@gmail.com>
2019-11-28 04:26:00 +0600

    login route complete

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const User = require('../models/user');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const bcrypt = require('bcrypt');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
       cb(null,'./backend/images/users')
   },
   filename: (req, file, cb) => {
-      cb(null, new Date().toDateString() + file.originalname);
+      cb(null, new Date().toISOString() + file.originalname);
   }
 });
 const fileFilter = (req, file, cb) => {
@@ -29,34 +30,82 @@ const upload = multer({
 
 router.post('/signup', upload.single('image'), (req, res, next) => {
   console.log(req.file);
-  const user = new User({
-    _id: mongoose.Types.ObjectId(),
-    email: req.body.email,
-    password: req.body.password,
-    name: {
-      first: req.body.firstName,
-      last: req.body.lastName
-    },
-    imagePath : (typeof req.file == "undefined") ? null : req.file.path,
-    //imagePath: req.file.path,
-    contactNumber: req.body.number,
-    address: req.body.address
-  });
-  user.save()
-  .then((result) => {
-      res.status(200).json({
-        message: 'user created successfully',
-        user: result
-      })
-  })
-  .catch((err) => {
-    res.status(500).json({
-      message: err
+  
+  User.find({ email: req.body.email})
+    .then(user => {
+      if(user.length > 0){
+        return res.status(500).json({
+          message: "Mail exists"
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10)
+          .then(hash => {
+            const user = new User({
+              _id: mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash,
+              name: {
+                first: req.body.firstName,
+                last: req.body.lastName
+              },
+              imagePath : (typeof req.file == "undefined") ? null : req.file.path,
+              //imagePath: req.file.path,
+              contactNumber: req.body.number,
+              address: req.body.address
+            });
+            user.save()
+              .then(user => {
+                res.status(200).json({
+                  message: 'User Created',
+                  user: user
+                });
+              })
+              .catch(err => {
+                return res.status(500).json({
+                  message: err
+                });
+              });
+        });
+      }
+    })
+    .catch(err => {
+      return res.status(500).json({
+        message: err
+      });
     });
-  });
   
 });
 
+router.post('/login', (req, res, err) => {
+    User.findOne({ email: req.body.email})
+    .then(user => {
+      //console.log(user);
+      if(!user){
+        return res.status(505).json({
+          message: 'User not found'
+        });
+      } else {
+        console.log(user.password);
+        bcrypt.compare(req.body.password, user.password)
+          .then(match => {
+             // console.log(match);
+              if(!match){
+                return res.status(505).json({
+                  message: 'Password not match'
+                });
+              }
+              res.status(200).json({
+                message: 'Successfull login'
+              });
+          })
+          .catch(err => {
+            return res.status(505).json({
+                message: err +"XX"
+              }); 
+          });
+      }
+    })
+});
 module.exports = router;
 
 
