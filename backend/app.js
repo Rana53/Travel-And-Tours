44165aceb0b3cacfcb8baa38733e7860ab449d5d
Rana53 <ranajahid.cseru@gmail.com>
2019-11-28 01:21:35 +0600

    mongoose mongoDB driver database connected and user schema created

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
 const userRouter = require('./routes/user');
 
+const mongoose = require('mongoose');
+const url = 'mongodb+srv://tyro-travel-and-tour:'+
+              process.env.MONGO_ATLAS_PW +
+              '@cluster0-7ciui.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true})
+  .then(() => {
+    console.log('Database connected');
+    })
+  .catch(() => {
+    console.log('Connection failed');
+  });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 
 app.use('/api/user', userRouter);
 
---
+++ backend/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  imagePath: {
+    type: String,
+    required: false,
+    default: null
+  },
+  isAdmin: {
+      type: Boolean,
+      required: false,
+      default: false
+  }
+});
+
+userSchema.plugin(uniqueValidator);
+module.exports = mongoose.model('User', userSchema);
